ef377ebadc201f7331d9ddf9898cc4b2a11d20f2
KUNG HSIU TING <62473938+timoxiu-hung@users.noreply.github.com>
2021-12-13 19:59:49 +0800

    Delete models directory

--- models/articles/request_other.py
+++
@@ -1,9 +0,0 @@
-others = {"2": "/tag/artificial-intelligence?source=topics_v2---------0-----------artificial_intelligence-------28-------------",
-          "3": "/tag/data-science?source=topics_v2---------1-----------data_science-------28-------------",
-          "4": "/tag/deep-learning?source=topics_v2---------2-----------deep_learning-------28-------------",
-          "5": "/tag/python?source=topics_v2---------3-----------python-------28-------------",
-          "6": "/tag/ai?source=topics_v2---------4-----------ai-------28-------------",
-          "7": "/tag/technology?source=topics_v2---------5-----------technology-------28-------------",
-          "8": "/tag/programming?source=topics_v2---------6-----------programming-------28-------------"}
-
-print(others)
